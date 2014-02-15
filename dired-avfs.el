5ae89a582c259a00bee285d20d4cad8d4e44f2a8
Matus Goljer <dota.keys@gmail.com>
2014-02-15 13:11:19 +0100

    [avfs] Update commentary

--- dired-avfs.el
+++ dired-avfs.el
@@ -24,9 +24,19 @@
 
 ;;; Commentary:
 
-;; Adds support for seamless browsing
+;; Adds AVFS (http://avf.sourceforge.net/) support for seamless archive
+;; browsing.  This extension therefore depends on the presence of `avfsd'
+;; on your system.  In debian-derived distributions you can usually do
+;;
+;;     apt-get install avfs
+;;
+;; `avfs' is probably also available for Mac OS.  You're out of luck on
+;; Windows, sorry.
 
-;; See https://github.com/Fuco1/dired-hacks for the entire collection
+;; Once the daemon is installed, run it with `mountavfs' and everything
+;; "Should Just Work^TM".
+
+;; See https://github.com/Fuco1/dired-hacks for the entire collection.
 
 ;;; Code:
 
