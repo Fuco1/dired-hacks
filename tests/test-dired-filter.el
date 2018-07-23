fa1e777ccac13bc975e33c1da08e5c024c9803e3
Matus Goljer <matus.goljer@gmail.com>
2018-07-23 12:53:05 +0200

    [buttercup] Use built-in matcher instead of or own

--- tests/test-dired-filter.el
+++ tests/test-dired-filter.el
@@ -23,13 +23,6 @@
      (dired-filter-group-mode 1)
      ,@body))
 
-(buttercup-define-matcher :to-equal-as-string-set (a b)
-  (let ((a-sorted (-sort 'string< a))
-        (b-sorted (-sort 'string< b)))
-    (if (equal a-sorted b-sorted)
-        (cons t (format "Expected %S not to `equal' %S" a-sorted b-sorted))
-      (cons nil (format "Expected %S to `equal' %S" a-sorted b-sorted)))))
-
 (buttercup-define-matcher :to-be-file (file)
   (if (file-regular-p file)
       (cons t (format "Expected %S not to be a file" file))
@@ -54,43 +47,43 @@
     (it "should hide dotfiles we don't want to see"
       (assess-with-filesystem '(".foo" "bar")
         (with-dired '((dot-files))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar"))))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar"))))))
 
   (describe "Dired name filter"
 
     (it "should keep lines matching the name"
       (assess-with-filesystem '("foo.o" "bar.a" "bar.h" "quux.c")
         (with-dired '((name . "bar"))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.a" "bar.h"))))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.a" "bar.h"))))))
 
   (describe "Dired regexp filter"
 
     (it "should keep files matching the name as regexp"
       (assess-with-filesystem '("foo.o" "bar.a" "bar.h" "quux.c")
         (with-dired '((name . "b.r\\."))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.a" "bar.h")))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.a" "bar.h")))))
 
     (it "should use case-sensitive match if qualifier contains upper-case letters"
       (assess-with-filesystem '("foo.o" "FOO.o")
         (with-dired '((name . "F"))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("FOO.o")))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("FOO.o")))))
 
     (it "should keep files or directories matching the name as regexp"
       (assess-with-filesystem '("foo.o" "bar.a" "bar.h" "quux.c" "bur.d/")
         (with-dired '((name . "b.r\\."))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.a" "bar.h" "bur.d")))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.a" "bar.h" "bur.d")))))
 
     (it "should be able to match extensions"
       (assess-with-filesystem '("foo.o" "bar.a" "bar.h" "quux.c")
         (with-dired '((name . "\\.a$"))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.a"))))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.a"))))))
 
   (describe "Dired omit filter"
 
     (it "should hide ignored files"
       (assess-with-filesystem '("bar.o" "bar.a" "bar.h" "bar.c")
         (with-dired '((omit))
-          (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.h" "bar.c"))))))
+          (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.h" "bar.c"))))))
 
   (describe "Dired and meta-filter"
 
@@ -99,7 +92,7 @@
       (it "should keep lines matching all the filters"
         (assess-with-filesystem '("foo" "bar" "bax/" "qux/")
           (with-dired '((file) (name . "bar"))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar")))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar")))))
 
       (it "should commute"
         (assess-with-filesystem '("foo" "bar" "bax/" "qux/")
@@ -107,19 +100,19 @@
                         (dired-utils-get-all-files :local)))
                 (other (with-dired '((directory) (name . "bax"))
                          (dired-utils-get-all-files :local))))
-            (expect this :to-equal-as-string-set other))))
+            (expect this :to-have-same-items-as other))))
 
       (it "should work with more than two filters"
         (assess-with-filesystem '("foo" "bar" "bax/" "qux/" "bar.c" "bar.h" "barfux.c" "barbara/")
           (with-dired '((name . "bar") (file) (extension . "c"))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.c" "barfux.c"))))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.c" "barfux.c"))))))
 
     (describe "Combining positive and negative filters"
 
       (it "should keep lines matching positive filters after removing those matched by negative filters"
         (assess-with-filesystem '(".bar" ".foo" "foo" "bar")
           (with-dired '((dot-files) (name . "bar"))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar")))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar")))))
 
       (it "should commute"
         (assess-with-filesystem '("bar.o" "bar.a" "bar.h" "bar.c" "foo.h" "foo.c" "foo.o")
@@ -127,19 +120,19 @@
                         (dired-utils-get-all-files :local)))
                 (other (with-dired '((omit) (name . "bar"))
                          (dired-utils-get-all-files :local))))
-            (expect this :to-equal-as-string-set other))))
+            (expect this :to-have-same-items-as other))))
 
       (it "should work as two positive filters if we negate the negative one"
         (assess-with-filesystem '(".bar" ".foo" "foo" "bar")
           (with-dired '((not (dot-files)) (name . "bar"))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '(".bar"))))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '(".bar"))))))
 
     (describe "Combining negative filters"
 
       (it "should remove lines matching any of the filters (deMorgan's law)"
         (assess-with-filesystem '(".bar" ".foo" "foo.o" "bar")
           (with-dired '((omit) (dot-files))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar")))))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar")))))))
 
   (describe "Dired or meta-filter"
 
@@ -148,7 +141,7 @@
       (it "should keep lines matching either of the filters"
         (assess-with-filesystem '("bar.o" "bar.a" "bar.h" "bar.c" "foo.h" "foo.c" "foo.o")
           (with-dired '((or (extension . "o") (name . "bar")))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar.a" "bar.o" "foo.o" "bar.h" "bar.c"))))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar.a" "bar.o" "foo.o" "bar.h" "bar.c"))))))
 
     (describe "Combining negative and positive filters"
 
@@ -156,7 +149,7 @@
         (assess-with-filesystem '(".bar" ".foo" "foo" "bar")
           ;; throw out all dotfiles except those having bar in the name
           (with-dired '((or (dot-files) (name . "bar")))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar" "foo" ".bar")))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar" "foo" ".bar")))))
 
       (it "should commute"
         (assess-with-filesystem '("bar.a" "foo.o" "foo" "bar")
@@ -165,7 +158,7 @@
                         (dired-utils-get-all-files :local)))
                 (other (with-dired '((or (omit) (name . "bar")))
                          (dired-utils-get-all-files :local))))
-            (expect this :to-equal-as-string-set other)))))
+            (expect this :to-have-same-items-as other)))))
 
 
     (describe "Combining negative filters"
@@ -174,7 +167,7 @@
         (assess-with-filesystem '(".bar.o" ".foo.txt" "foo" "bar")
           ;; throw out all dotfiles with "omit" extension
           (with-dired '((or (dot-files) (omit)))
-            (expect (dired-utils-get-all-files :local) :to-equal-as-string-set '("bar" "foo" ".foo.txt"))))))))
+            (expect (dired-utils-get-all-files :local) :to-have-same-items-as '("bar" "foo" ".foo.txt"))))))))
 
 (describe "Dired Filter Groups"
 
@@ -184,8 +177,8 @@
                             ("Directories" (directory))
                             ("LaTeX" (extension "tex" "bib"))))
         (let ((groups (dired-filter-group-get-groups)))
-          (expect (gethash "Directories" groups) :to-equal-as-string-set '("." ".." "foo" "bar"))
-          (expect (gethash "LaTeX" groups) :to-equal-as-string-set '("baz.tex" "baz.bib"))))))
+          (expect (gethash "Directories" groups) :to-have-same-items-as '("." ".." "foo" "bar"))
+          (expect (gethash "LaTeX" groups) :to-have-same-items-as '("baz.tex" "baz.bib"))))))
 
   (it "should not create empty drawers when groups overlap [#57]"
     (assess-with-filesystem '("a.py" "b.py" "regular" "another")
@@ -193,12 +186,12 @@
                             ("Python" (extension "py"))
                             ("B" (name . "b"))))
         (let ((groups (dired-filter-group-get-groups)))
-          (expect (gethash "B" groups) :to-equal-as-string-set '("b.py"))
-          (expect (gethash "Python" groups) :to-equal-as-string-set '("a.py"))))
+          (expect (gethash "B" groups) :to-have-same-items-as '("b.py"))
+          (expect (gethash "Python" groups) :to-have-same-items-as '("a.py"))))
 
       (with-dired-groups '(("default"
                             ("B" (name . "b"))
                             ("Python" (extension "py"))))
         (let ((groups (dired-filter-group-get-groups)))
-          (expect (gethash "B" groups) :to-equal-as-string-set nil)
-          (expect (gethash "Python" groups) :to-equal-as-string-set '("a.py" "b.py")))))))
+          (expect (gethash "B" groups) :to-have-same-items-as nil)
+          (expect (gethash "Python" groups) :to-have-same-items-as '("a.py" "b.py")))))))
