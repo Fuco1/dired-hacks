368277b04ad0a224f377f83b1fc28d98076e474b
Dale Sedivec <dale@codefu.org>
2018-11-10 12:45:28 -0600

    Correct un-narrowing when hitting backspace
    `dired-narrow--restore` is called from `dired-narrow--update` when
    it's updating the narrowing of the buffer.  However, if you type
    some character that doesn't occur in any file name (ex. `{`), then
    backspace it, then type a character that does occur in a file
    name---or just hit RET for no narrowing at all---all files will be
    narrowed away when exiting dired-narrow because the `:dired-narrow`
    property still exists on them from when you typed `{`.
    
    This change just has `dired-narrow--restore` remove the
    `:dired-narrow` property at the same tim as it removes the `invisible`
    property.

--- dired-narrow.el
+++ dired-narrow.el
@@ -174,7 +174,8 @@ when `dired-narrow-exit-when-one-left' and `dired-narrow-enable-blinking' are tr
 (defun dired-narrow--restore ()
   "Restore the invisible files of the current buffer."
   (let ((inhibit-read-only t))
-    (remove-text-properties (point-min) (point-max) '(invisible))
+    (remove-list-of-text-properties (point-min) (point-max)
+                                    '(invisible :dired-narrow))
     (when (fboundp 'dired-insert-set-properties)
       (dired-insert-set-properties (point-min) (point-max)))))
 
