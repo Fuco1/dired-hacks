0859647c10889bbedb80a882c390f0f6dcc7780d
Matthew Sojourner Newton <matt@mnewton.com>
2019-05-21 15:09:34 -0700

    Corrected comments and minor mode docstring

--- dired-hacks-utils.el
+++ dired-hacks-utils.el
@@ -31,6 +31,9 @@
 ;; * `dired-hacks-next-file' - go to next file, skipping empty and non-file lines
 ;; * `dired-hacks-previous-file' - go to previous file, skipping empty
 ;;   and non-file lines
+;; * `dired-utils-format-information-line-mode' - Format the information
+;; (summary) line file sizes to be human readable (e.g. 1GB instead of 1048576).
+
 
 ;; See https://github.com/Fuco1/dired-hacks for the entire collection
 
@@ -258,7 +261,7 @@ line."
                  "probably the same" "different"))))
 
 (define-minor-mode dired-utils-format-information-line-mode
-  "Toggle formatting "
+  "Toggle formatting of disk space in the Dired information line."
   :group 'dired-utils
   :lighter ""
   (if dired-utils-format-information-line-mode
